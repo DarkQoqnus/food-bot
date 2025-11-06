86cfc5750e89762c5737c5ac4cdc005f432cb974
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-11-06 16:43:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from telethon import TelegramClient
+import asyncio
 
+API_ID = 28175931
+API_HASH = '2455aeda9d813346dd8e12d06c331e12'
+PHONE_NUMBER = '+989156707283'
+
+async def main():
+    client = TelegramClient('session', API_ID, API_HASH)
+    await client.start(PHONE_NUMBER)
+    
+    session_string = client.session.save()
+    print("\n" + "="*50)
+    print("SESSION_STRING شما:")
+    print(session_string)
+    print("="*50)
+    
+    await client.disconnect()
+
+asyncio.run(main())
