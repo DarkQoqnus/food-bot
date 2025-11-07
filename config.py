7613fcd3e33937419ad221e38a1a6cbe021b8bb4
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-11-07 22:34:28 +0330

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ YOUR_USER_ID = int(os.environ['YOUR_USER_ID'])
 SESSION_STRING = os.environ['SESSION_STRING']
 GROUP_ID = int(os.environ['GROUP_ID'])
 
+# وضعیت ربات
+bot_status = "on"
+current_filter = "همه"
+message_stats = {"total": 0, "success": 0}
+
 # کلمات کلیدی
 LOCATION_FILTERS = {
     "سلف": ["سلف", "سلفی", "غذای سلف", "سلف دانشگاه", "مرکزی"],
     "حافظ": ["حافظ", "حافظی", "غذای حافظ"],
     "همه": ["فروش", "میفروشم", "فروشی"]
 }
-
-# وضعیت ربات
-bot_status = "on"
-current_filter = "همه"
-message_stats = {"total": 0, "success": 0}
