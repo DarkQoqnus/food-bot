a56ce8dd296b43c4697cf1f66d62d1e774e1022d
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-11-07 13:11:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import requests
 
 # تنظیمات
 API_ID = int(os.environ['API_ID'])
-API_HASH = os.environ['API_HASH'])
-BOT_TOKEN = os.environ['BOT_TOKEN'])
+API_HASH = os.environ['API_HASH']
+BOT_TOKEN = os.environ['BOT_TOKEN']
 YOUR_USER_ID = int(os.environ['YOUR_USER_ID'])
-SESSION_STRING = os.environ['SESSION_STRING'])
+SESSION_STRING = os.environ['SESSION_STRING']
 GROUP_ID = int(os.environ['GROUP_ID'])
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
