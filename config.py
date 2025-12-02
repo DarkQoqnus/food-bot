5f53dd87a899114a1f5ff477147816ededf5fec8
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-12-03 02:18:32 +0330

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,6 @@ YOUR_USER_ID = int(os.environ['YOUR_USER_ID'])
 SESSION_STRING = os.environ['SESSION_STRING']
 GROUP_ID = int(os.environ['GROUP_ID'])
 
-# لیست اکانت‌ها
-ACCOUNTS = [
-    {
-        'session_string': os.environ['SESSION_STRING'],
-        'name': '@oxgangster',
-        'active': True,
-        'phone': '+989156707283'
-    }
-    # اکانت‌های جدید اینجا اضافه میشن
-]
-
 # وضعیت ربات
 bot_status = "on"
 current_filter = "همه"
@@ -26,7 +15,7 @@ message_stats = {"total": 0, "success": 0}
 
 # کلمات کلیدی
 LOCATION_FILTERS = {
-    "سلف": ["سلف", "سلفی", "غذای سلف", "سلف دانشگاه", "مرکزی"],
-    "حافظ": ["حافظ", "حافظی", "غذای حافظ"],
-    "همه": ["فروش", "میفروشم", "فروشی"]
+    "سلف": ["سلف", "سلفی", "غذای سلف", "سلف دانشگاه", "سلف اصلی"],
+    "حافظ": ["حافظ", "حافظی", "غذای حافظ", "رستوران حافظ", "حافظ مرکزی"],
+    "همه": ["فروش", "غذا", "میفروشم", "فروشی", "سلف", "حافظ"]
 }
