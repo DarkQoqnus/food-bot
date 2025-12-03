ce9e7a55e9b7a8b13a06e72b6711fc631434118d
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-12-03 14:41:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# main.py
-from telethon import TelegramClient, events
-import config
+import os
 import asyncio
+from telethon import TelegramClient, events
 from manager_bot import report_to_manager
 
-client = TelegramClient("session", config.API_ID, config.API_HASH)
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+SESSION_STRING = os.environ.get("SESSION_STRING")
+GROUP_ID = int(os.environ.get("GROUP_ID"))
+
+# کلمات کلیدی پیش‌فرض
+FILTER_WORDS = [
+    "سلف", "سلف فروشی", "سلف میفروشم",
+    "حافظ", "حافظ فروشی", "حافظ میفروشم"
+]
+
+client = TelegramClient("session", API_ID, API_HASH)
 client.start()
 
 active = True
-filter_words = config.FILTER_WORDS
+filter_words = FILTER_WORDS
 
-@client.on(events.NewMessage(chats=config.GROUP_ID))
+@client.on(events.NewMessage(chats=GROUP_ID))
 async def handler(event):
     global active, filter_words
     if not active:
