d087fc3641fa9b324940462f0f486db93f148e57
DarkQoqnus <141250174+DarkQoqnus@users.noreply.github.com>
2025-12-03 14:41:38 +0330

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-API_ID = 123456          # از my.telegram.org بگیر
-API_HASH = "your_api_hash"
-SESSION_STRING = "your_session_string"  # با Telethon تولید میشه
-
-BOT_TOKEN = "your_bot_token"  # از BotFather
-GROUP_ID = -1001234567890     # آیدی گروه غذا
-FILTER_WORDS = ["سلف", "سلف فروشی", "سلف میفروشم",
-                "حافظ", "حافظ فروشی", "حافظ میفروشم"]
